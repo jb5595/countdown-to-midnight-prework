44d6e4dd5a64a94267f8a21729b2c5cbc7f35d44
Jacob Berman <jacobberman1995@gmail.com>
2018-08-08 01:29:20 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,11 @@ def countdown (number)
   puts 'HAPPY NEW YEAR!'
 end
 
-def countdown_with_sleep(number)
-    while number > 0 
-    puts "#{number} SECONDS(S)!"
-     number -= 1
-     sleep(1)
-    end 
-  puts 'HAPPY NEW YEAR!'
-end
+# def countdown_with_sleep(number)
+#   while number > 0 
+#     puts "#{number} SECONDS(S)!"
+#     number -= 1
+#     sleep(1)
+#     end 
+#   puts 'HAPPY NEW YEAR!'
+# end
